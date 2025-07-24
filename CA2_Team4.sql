cf8f5c688b64fd34858a2c29a4660e6e56f075b5
jcyc-commits <joshchanyc@gmail.com>
2025-07-24 21:54:40 +0800

    Add files via upload

--- CA2_Team4.sql
+++ CA2_Team4.sql
@@ -33,6 +33,7 @@ CREATE TABLE `admin` (
   `email` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
   `contact` varchar(10) NOT NULL,
+  `image` varchar(1000) NOT NULL,
   PRIMARY KEY (`adminId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -63,6 +64,7 @@ CREATE TABLE `student` (
   `email` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
   `contact` varchar(10) NOT NULL,
+  `image` varchar(1000) NOT NULL,
   PRIMARY KEY (`studentId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -97,6 +99,7 @@ CREATE TABLE `teacher` (
   `teachingcert` varchar(1000) NOT NULL,
   `teachingGrade` varchar(6) NOT NULL,
   `resume` varchar(1000) NOT NULL,
+  `image` varchar(1000) NOT NULL,
   PRIMARY KEY (`teacherId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -119,4 +122,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-07-24 21:43:51
+-- Dump completed on 2025-07-24 21:53:11
