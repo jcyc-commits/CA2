f1a875ca5b6e72d59b79f801403c86b98199dca1
jcyc-commits <joshchanyc@gmail.com>
2025-07-24 21:40:39 +0800

    Add files via upload

--- CA2_Team4.sql
+++ CA2_Team4.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `ca2_team4` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `ca2_team4`;
 -- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
 --
 -- Host: localhost    Database: ca2_team4
@@ -57,6 +59,7 @@ CREATE TABLE `student` (
   `password` varchar(255) NOT NULL,
   `name` varchar(45) NOT NULL,
   `dob` date NOT NULL,
+  `grade` varchar(2) NOT NULL,
   `email` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
   `contact` varchar(10) NOT NULL,
@@ -92,6 +95,7 @@ CREATE TABLE `teacher` (
   `subject` varchar(45) NOT NULL,
   `payperhour` decimal(1,0) NOT NULL,
   `teachingcert` varchar(1000) NOT NULL,
+  `teachingGrade` varchar(6) NOT NULL,
   `resume` varchar(1000) NOT NULL,
   PRIMARY KEY (`teacherId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -115,4 +119,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-07-24 17:55:27
+-- Dump completed on 2025-07-24 21:39:43
